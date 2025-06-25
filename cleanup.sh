eaa6f38c9705fe067f266746878bbf312baaf57e
amonclus <amonclus89@gmail.com>
2025-06-25 10:56:21 +0200

    k8s

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# cleanup.sh
+
+echo "Stopping port-forward processes..."
+pkill -f "kubectl port-forward"
+
+echo "Deleting Kubernetes resources..."
+kubectl delete deployment movies-api-deployment --ignore-not-found=true
+kubectl delete deployment mysql-deployment --ignore-not-found=true
+kubectl delete service movies-api-service --ignore-not-found=true
+kubectl delete service mysql-service --ignore-not-found=true
+
+echo "Verifying cleanup..."
+kubectl get all
+
+echo "Cleanup complete!"
